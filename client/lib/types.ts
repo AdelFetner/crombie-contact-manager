960ea89d702b85822253d64bb8d7405d9da3d3b7
Adel Fetner <adelfetner@gmail.com>
2025-03-11 22:04:12 -0300

    chore: did more error handling

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -2,10 +2,19 @@ import { z } from "zod";
 
 // the schema for the contactForm
 export const formSchema = z.object({
-    firstName: z.string().min(2),
-    lastName: z.string().min(2),
-    email: z.string().email(),
-    phone: z.string().min(8).max(16),
+    firstName: z
+        .string()
+        .min(2, { message: "Name is too short" })
+        .max(30, { message: "Name is too long" }),
+    lastName: z
+        .string()
+        .min(2, { message: "Last Name is too short" })
+        .max(30, { message: "Last Name is too long" }),
+    email: z.string().email({ message: "Invalid email" }),
+    phone: z
+        .string()
+        .min(8, { message: "Phone number is too short" })
+        .max(16, { message: "Phone number is too long" }),
     company: z.string().optional(),
     role: z.string().optional(),
     notes: z.string().optional(),
